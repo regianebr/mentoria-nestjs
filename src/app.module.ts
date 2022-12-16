0f5b0a404a8495bd55db3527a228be827a26ad7d
regianebr <regiane.brito@gavb.com.br>
2022-12-15 22:48:10 -0300

    segundo envio db ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: 'mentoria-nestjs'
+    })],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+
+export class AppModule { }
+
+
+
